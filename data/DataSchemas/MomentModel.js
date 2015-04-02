ca1f96bf8667d1c9729cc00c4ca4d215ab15a5e9
MengJaeSuk <MengJaeSuk@gmail.com>
2015-04-02 14:54:09 +0800

    moment model

--- data/DataSchemas/MomentModel.js
+++ data/DataSchemas/MomentModel.js
@@ -1,3 +1,21 @@
 /**
  * Created by cmm on 4/2/15.
  */
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var schema = new Schema({
+    author: {
+        _id: String,
+        head: String,
+        account: String },
+    pictures:[{path:String}],
+    content:String,
+    date: String,
+    likeNum: Number,
+    commentNum: Number,
+    flag: Boolean
+});
+
+module.exports = schema;
