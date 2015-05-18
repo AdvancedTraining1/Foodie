a38f8f3c6a66bd3f8df7155920281d0c865dab5e
mengchi6131 <mengchi6131@gmail.com>
2015-05-18 11:16:10 +0800

    friend part

--- controller/friendsHandler.js
+++ controller/friendsHandler.js
@@ -22,6 +22,7 @@ FriendsHandler.saveFriendMessage=function(req,res){
     var friendAccount =req.param('friendAccount');
     var friendHead =req.param('friendHead');
     var message =req.param('message');
+    var token = req.param('token');
 
     var to = {};
     to._id = friendId;
@@ -39,7 +40,8 @@ FriendsHandler.saveFriendMessage=function(req,res){
             from:from,
             to:to,
             status:"0",
-            message:message
+            message:message,
+            date:getTime()
         });
 
         FriendMessageDao.create(FriendMessage,function(error, friendMessage){
@@ -65,6 +67,7 @@ FriendsHandler.dealFriendMessage = function(req,res) {
     console.log(friendId)
     var friendAccount = req.param('friendAccount');
     var friendHead = req.param('friendHead');
+    var token = req.param('token');
 
     var friends = {};
     friends._id = friendId;
@@ -162,6 +165,7 @@ FriendsHandler.dealFriendMessage = function(req,res) {
     }
 
     FriendsHandler.getFriendMessage = function (req, res) {
+        var token = req.param('token');
 
         AccessToken.userActionWithToken(token, res, function (user) {
             var id = user.id;
@@ -187,23 +191,27 @@ FriendsHandler.dealFriendMessage = function(req,res) {
 
     FriendsHandler.showAllFriends = function (req, res) {
         console.log("show all friend of user");
+        var token = req.param('token');
 
-        var userId = req.param('userId');
-        console.log(userId);
+        //var userId = req.param('userId');
+        //console.log(userId);
+        AccessToken.userActionWithToken(token, res, function (user) {
+            var id = user.id;
 
-        UserDao.getUserById(userId, function (err, user) {
-            if (err) {
-                res.json(500, {message: err.toString()});
-                return;
-            }
-            if (!user) {
-                res.json(404, {message: "This user not exit"});
-                return;
+            UserDao.getUserById(id, function (err, user) {
+                if (err) {
+                    res.json(500, {message: err.toString()});
+                    return;
+                }
+                if (!user) {
+                    res.json(404, {message: "This user not exit"});
+                    return;
 
-            }
+                }
 
-            res.json(200, {friendList: user.friends, num: user.friends_count});
+                res.json(200, {friendList: user.friends, num: user.friends_count});
 
+            })
         })
     }
 
@@ -266,61 +274,56 @@ FriendsHandler.dealFriendMessage = function(req,res) {
         var friendId = req.param('friendId');
         var friendAccount = req.param('friendAccount');
         var friendHead = req.param('friendHead');
-
-        //var friendId = "551d7bbe1fb802ae1c438f15"//req.param('friendId');
-        //var friendAccount ="bbb" //req.param('friendAccount');
-        //var friendHead ="/head/defaulthead.jpeg" //req.param('friendHead');
+        var token = req.param('token');
 
         var friends = {};
         friends._id = friendId;
         friends.account = friendAccount;
         friends.head = friendHead;
 
-        var userId = "551f50c59561511112bde212"; //req.session.user_id;
-        var userAccount = "aaa"; //req.session.account;
-        var userHead = "/head/defaulthead.jpeg"; //req.session.head;
-        //
-        //var userId = req.session.user_id;
-        //var userAccount = req.session.account;
-        //var userHead = req.session.head;
-        var me = {}
-        me._id = userId;
-        me.account = userAccount;
-        me.head = userHead;
+        AccessToken.userActionWithToken(token, res, function (user) {
 
-        FriendDao.deleteFriend(friendId, me, function (err, user) {
-            if (err) {
-                res.json(500, {message: err.toString()});
-                return;
-            }
-            else if (!user) {
-                res.json(404, {message: "Not found1."});
-                return;
-            } else {
-                FriendDao.deleteFriend(userId, friends, function (err, user2) {
-                    if (err) {
-                        res.json(500, {message: err.toString()});
-                        return;
-                    }
-                    if (!user) {
-                        res.json(404, {message: "Not found2."});
-                        return;
-                    }
-                    res.json(200, {message: 'delete friends success', user: user2});
+            var userId = user._id;
+            var me = {};
+            me._id = user._id;
+            me.account = user.account;
+            me.head = user.head;
 
-                })
-            }
+            FriendDao.deleteFriend(friendId, me, function (err, user) {
+                if (err) {
+                    res.json(500, {message: err.toString()});
+                    return;
+                }
+                else if (!user) {
+                    res.json(404, {message: "Not found1."});
+                    return;
+                } else {
+                    FriendDao.deleteFriend(userId, friends, function (err, user2) {
+                        if (err) {
+                            res.json(500, {message: err.toString()});
+                            return;
+                        }
+                        if (!user) {
+                            res.json(404, {message: "Not found2."});
+                            return;
+                        }
+                        res.json(200, {message: 'delete friends success', user: user2});
+
+                    })
+                }
+            })
         })
+
+
 
     }
 
-    FriendsHandler.searchFriends = function (req, res) {
-
+    FriendsHandler.searchNewFriends = function (req, res) {
 
         var pageNo = req.param('pageNo');
         var pageSize = req.param('pageSize');
         var queryStr = req.param('queryStr');//"b" //
-        var userid = req.session.user_id;//"551d7bb281e3449e1c1c6600"//
+    //    var userid = req.session.user_id;//"551d7bb281e3449e1c1c6600"//
 
         FriendDao.searchFriends(pageNo, pageSize, queryStr, function (err, users) {
             if (err) {
@@ -331,7 +334,6 @@ FriendsHandler.dealFriendMessage = function(req,res) {
                 res.json(200, {message: "Not found."});
                 return;
             }
-
             res.json(200, {friendList: users});
 
         });
@@ -339,23 +341,55 @@ FriendsHandler.dealFriendMessage = function(req,res) {
     }
 
 
+
     FriendsHandler.checkFriend = function (req, res) {
-        FriendDao.check(userId, friendId, function (err, result) {
+        var friendId = req.param('friendId');
 
-            var isFriend = false;
+        AccessToken.userActionWithToken(token, res, function (user) {
 
-            if (err) {
-                res.json(500, {message: err.toString()});
-                return;
+            var userId = user._id;
+            FriendDao.check(userId, friendId, function (err, result) {
 
-            }
-            if (!result) {
-                isFriend = false;
-            } else
-                isFriend = true;
+                var isFriend = false;
 
-            res.json(200, {isFriend: isFriend});
+                if (err) {
+                    res.json(500, {message: err.toString()});
+                    return;
 
+                }
+                if (!result) {
+                    isFriend = false;
+                } else
+                    isFriend = true;
+
+                res.json(200, {isFriend: isFriend});
+
+            });
+        });
+
+    }
+
+    FriendsHandler.searchMyFriend = function(req,res){
+        AccessToken.userActionWithToken(token, res, function (user) {
+
+            var userId = user._id;
+            FriendDao.check(userId, friendId, function (err, result) {
+
+                var isFriend = false;
+
+                if (err) {
+                    res.json(500, {message: err.toString()});
+                    return;
+
+                }
+                if (!result) {
+                    isFriend = false;
+                } else
+                    isFriend = true;
+
+                res.json(200, {isFriend: isFriend});
+
+            });
         });
 
     }
@@ -389,7 +423,7 @@ FriendsHandler.dealFriendMessage = function(req,res) {
         });
     }
 
-    function logTime() {
+    function getTime() {
         var data = new Date().format('yyyy-MM-dd hh:mm:ss');
         console.log(data);
         return data;
--- routes/friends.js
+++ routes/friends.js
@@ -5,8 +5,12 @@ var friend = require('./../controller/friendsHandler');
 
 module.exports = function (app) {
 
+    app.post('/service/friend/saveFriendMessage',friend.saveFriendMessage);
+    app.get('/service/friend/getFriendMessage',friend.getFriendMessage);
+    app.post('/service/friend/dealFriendMessage',friend.dealFriendMessage);
+
     app.get('/service/friend/listFriend', friend.showAllFriends);
-    app.get('/service/friend/searchFriend',friend.searchFriends);
+    app.get('/service/friend/searchNewFriend',friend.searchNewFriends);
     app.post('/service/friend/add', friend.addFriend);
     app.post('/service/friend/delete', friend.deleteFriend);
     app.get('/service/friend/check', friend.checkFriend);
