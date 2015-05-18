fe891fe27ba05df2dc32e333958a28d1b714e02b
mengchi6131 <mengchi6131@gmail.com>
2015-05-18 11:26:36 +0800

    friend part

--- routes/friends.js
+++ routes/friends.js
@@ -6,7 +6,7 @@ var friend = require('./../controller/friendsHandler');
 module.exports = function (app) {
 
     app.post('/service/friend/saveFriendMessage',friend.saveFriendMessage);
-    app.get('/service/friend/getFriendMessage',friend.getFriendMessage);
+  /*  app.get('/service/friend/getFriendMessage',friend.getFriendMessage);
     app.post('/service/friend/dealFriendMessage',friend.dealFriendMessage);
 
     app.get('/service/friend/listFriend', friend.showAllFriends);
@@ -14,6 +14,6 @@ module.exports = function (app) {
     app.post('/service/friend/add', friend.addFriend);
     app.post('/service/friend/delete', friend.deleteFriend);
     app.get('/service/friend/check', friend.checkFriend);
-   // app.get('/service/friend/adduser', friend.adduser);
+   // app.get('/service/friend/adduser', friend.adduser);*/
 
 };
