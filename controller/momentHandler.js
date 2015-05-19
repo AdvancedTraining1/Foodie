a67f9cde59de10b871c32301e417994b5a5ec39d
MengJaeSuk <MengJaeSuk@gmail.com>
2015-05-19 20:05:27 +0800

    moment addMoment

--- controller/momentHandler.js
+++ controller/momentHandler.js
@@ -120,11 +120,6 @@ exports.addMoment = function(req,res){
                 }
             });
         });
-
-        res.writeHead(200, {
-            "Content-Type": "text/plain;charset=utf-8"
-        });
-        res.end("数据提交完毕");
     });
 
 
